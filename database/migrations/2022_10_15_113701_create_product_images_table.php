a0715b7111d8282d662d9317a2711e2481381d01
shavlenkov <pashashavlenkov@gmail.com>
2022-11-05 19:57:21 +0200

    [fix] added a default value of null to the filename field

--- database/migrations/2022_10_15_113701_create_product_images_table.php
+++ database/migrations/2022_10_15_113701_create_product_images_table.php
@@ -17,7 +17,7 @@ class CreateProductImagesTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('product_id')->unsigned();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
-            $table->string('filename');
+            $table->string('filename')->nullable();
             $table->timestamps();
         });
     }
