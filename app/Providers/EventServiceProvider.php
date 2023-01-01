4a4837e09221bfe0a6f8d9cf9913b01d4283fd8a
shavlenkov <pashashavlenkov@gmail.com>
2023-01-01 21:46:55 +0200

    [feat] registered events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,13 @@ use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
+use App\Events\OrderCreated;
+use App\Events\OrderStatusUpdated;
+
+use App\Listeners\SendMailOrderCreatedListener;
+use App\Listeners\SendMailOrderStatusUpdatedListener;
+
+
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -17,6 +24,12 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        OrderCreated::class => [
+            SendMailOrderCreatedListener::class,
+        ],
+        OrderStatusUpdated::class => [
+            SendMailOrderStatusUpdatedListener::class,
         ],
     ];
 
